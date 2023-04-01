b66a12420f2a73c0df1a11e365ccdc9d07edd2d8
Hernan Finucci <hfinucci@itba.edu.ar>
2023-04-01 12:12:49 -0300

    computer killer 😈

--- ovitoGenerator.py
+++ ovitoGenerator.py
@@ -24,8 +24,8 @@ def export_to_ovito(frame_file):
 
     pipeline.modifiers.append(simulation_cell)
 
-    export_file(pipeline, 'results_ovito1.0.dump', 'lammps/dump',
-                columns=["Particle Identifier", "Position.X", "Position.Y", "Position.Z", "Angle"],
+    export_file(pipeline, 'results_ovito1.1.dump', 'lammps/dump',
+                columns=["Position.X", "Position.Y", "Position.Z", "Color.R", "Color.G", "Color.B"],
                 multiple_frames=True, start_frame=0, end_frame=len(data_frame) - 1)
 
 
@@ -46,6 +46,25 @@ def direction_to_angle(direction: str) -> int:
     print("error de angulo")
     return 0
 
+def particle_list_to_color(particle_list: list) -> tuple:
+    density = particle_list.count(1)
+    if density == 0:
+        return 0, 1.0, 1.0
+    if density == 1:
+        return 0, 1.0, 0
+    if density == 2:
+        return 1.0, 1.0, 1.0
+    if density == 3:
+        return 1.0, 0.2, 0.9
+    if density == 4:
+        return 0.6, 0.9, 0.1
+    if density == 5:
+        return 1.0, 0.2, 0.9
+    if density == 6:
+        return 1.0, 1.0, 0.3
+
+color_rgb_list = []
+
 
 def get_particle_data(frame_file):
     frames = []
@@ -56,21 +75,28 @@ def get_particle_data(frame_file):
         next(frame)
         frame_lines = []
         for line in frame:
-            ll = line.split()
+            ll = line.split('\t')
             line_info = []
+            if len(ll) > 1:
+                # Parses the particle list of bits and converts it to a rgb value. Example, [1,0,1,0,0,1] -> (1.0, 1.0, 0,5)
+                color_rgb = particle_list_to_color(ll[3].replace("\n","").replace("[","").replace("]","").split(","))
+                color_rgb_list.append(color_rgb)
             for index in ll:
                 if index.isalpha():
                     index = direction_to_angle(index)
-                line_info.append(float(index))
+                if index.isnumeric():
+                    line_info.append(float(index))
             if len(line_info) > 1:
                 frame_lines.append(line_info)
             elif len(line_info) == 1:
                 # df = pd.DataFrame(np.array(frame_lines), columns=["id", "x", "y", "z", "speed", "angle", "radius"])
-                df = pd.DataFrame(np.array(frame_lines), columns=["id", "x", "y", "z", "angle"])
+                # df = pd.DataFrame(np.array(frame_lines), columns=["x", "y", "z", "color"])
+                df = pd.DataFrame(np.array(frame_lines), columns=["x", "y", "z"])
                 frames.append(df)
                 frame_lines = []
         # df = pd.DataFrame(np.array(frame_lines), columns=["id", "x", "y", "z", "speed", "angle", "radius"])
-        df = pd.DataFrame(np.array(frame_lines), columns=["id", "x", "y", "z", "angle"])
+        # df = pd.DataFrame(np.array(frame_lines), columns=["x", "y", "z", "color"])
+        df = pd.DataFrame(np.array(frame_lines), columns=["x", "y", "z"])
         frames.append(df)
     print(frames)
 
@@ -79,10 +105,14 @@ def get_particle_data(frame_file):
 
 def get_particles(data_frame):
     particles = Particles()
-    particles.create_property('Particle Identifier', data=data_frame.id)
+    # particles.create_property('Particle Identifier', data=data_frame.id)
     particles.create_property("Position", data=np.array((data_frame.x, data_frame.y, data_frame.z)).T)
     # particles.create_property('Radius', data=data_frame.radius)
-    particles.create_property('Angle', data=data_frame.angle)
+    # particles.create_property('Color', data=np.array(color_rgb_list))
+    # particles.create_property('Color', data=np.array(color_rgb_list[-5000:]))
+    particles.create_property('Color', data=np.array(color_rgb_list))
+    # particles.create_property('Color', data=(1.0, 1.0, 0))
+    # particles.create_property('Angle', data=data_frame.angle)
     # particles.create_property("Force", data=np.array(
     #     (np.cos(data_frame.angle) * data_frame.speed, np.sin(data_frame.angle) * data_frame.speed, data_frame.z)).T)
 
